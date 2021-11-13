00198c81835eb5e336f19d5e6812349f48c980e1
jiwon <73864010+nerisa2123@users.noreply.github.com>
2021-11-14 07:49:47 +0900

    재업로드

--- EndLine/app/src/main/java/com/example/endline_v1/EnterNumber.java
+++ EndLine/app/src/main/java/com/example/endline_v1/EnterNumber.java
@@ -30,7 +30,7 @@ public class EnterNumber extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), DirectlyAdd.class);
-                intent.putExtra("barcode_number", et_barcodeNum.getText().toString());
+                intent.putExtra("barcodeNum", et_barcodeNum.getText().toString());
                 startActivity(intent);
                 finish();
             }
